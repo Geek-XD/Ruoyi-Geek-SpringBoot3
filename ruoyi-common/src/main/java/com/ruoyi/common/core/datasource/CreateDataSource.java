8b9eaf40ed8468bedb29631a96e17fe943284123
dftre <3066417822@qq.com>
2025-11-25 15:10:02 +0800

    回归默认配置

--- ruoyi-common/src/main/java/com/ruoyi/common/core/datasource/CreateDataSource.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.common.core.datasource;
-
-import java.util.Properties;
-
-import javax.sql.CommonDataSource;
-
-public interface CreateDataSource {
-    CommonDataSource createDataSource(String name, Properties prop);
-}
