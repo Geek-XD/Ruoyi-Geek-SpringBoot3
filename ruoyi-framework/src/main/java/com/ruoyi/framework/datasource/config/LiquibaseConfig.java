9b5f46c15f308d34eea4769a4f040f612debeb10
dftre <3066417822@qq.com>
2025-11-26 20:09:10 +0800

    进一步优化初始化表

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/datasource/config/LiquibaseConfig.java
@@ -0,0 +1,33 @@
+package com.ruoyi.framework.datasource.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.ruoyi.framework.datasource.manager.DataSourceManager;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Autowired
+    private DataSourceManager dataSourceManager;
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        DriverManagerDataSource nativeDs = new DriverManagerDataSource();
+        DruidDataSource druidDataSource = (DruidDataSource) dataSourceManager.getPrimaryDataSource();
+        nativeDs.setUrl(druidDataSource.getUrl());
+        nativeDs.setUsername(druidDataSource.getUsername());
+        nativeDs.setPassword(druidDataSource.getPassword());
+        nativeDs.setDriverClassName(druidDataSource.getDriverClassName());
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(nativeDs);
+        liquibase.setChangeLog("classpath:liquibase/master.xml");
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+}
